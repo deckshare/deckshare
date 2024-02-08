cc04bd3065add70761d28adf2cf1815547e50da0
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-02-08 09:29:59 -0800

    Stub non-GET users controller actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,21 @@ class UsersController < ApplicationController
   def new
   end
 
-  def edit
+  def create
+    redirect_to action: :show
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    redirect_to action: :show
+  end
+
+  def destroy
+    redirect_to action: :new
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,13 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "User", type: :request do
-  describe "GET /" do
-    it "returns http success" do
-      get "/user"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "GET /new" do
     it "returns http success" do
       get "/user/new"
@@ -19,6 +12,41 @@ RSpec.describe "User", type: :request do
     it "returns http success" do
       get "/user/edit"
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /" do
+    it "returns http success" do
+      get "/user"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "PATCH /" do
+    it "redirects to show" do
+      patch "/user"
+      expect(response).to redirect_to(action: :show)
+    end
+  end
+
+  describe "PUT /" do
+    it "redirects to show" do
+      put "/user"
+      expect(response).to redirect_to(action: :show)
+    end
+  end
+
+  describe "DELETE /" do
+    it "redirects to new" do
+      delete "/user"
+      expect(response).to redirect_to(action: :new)
+    end
+  end
+
+  describe "POST /" do
+    it "redirects to show" do
+      post "/user"
+      expect(response).to redirect_to(action: :show)
     end
   end
 
