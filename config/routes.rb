130f311fb8e1fd84bf04b8af2657c971306fbb1a
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-02-08 12:07:04 -0800

    Generate sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "_up" => "rails/health#show", as: :rails_health_check
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resource :session, as: :sessions, only: %i[new create destroy]
   resource :user, as: :users
 
   # Defines the root path route ("/")
