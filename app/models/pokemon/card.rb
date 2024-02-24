e9f971c6aa3c810d041bd0e5d9d4cbaecf82eb22
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-02-23 17:45:59 -0800

    Fix set has_many cards association

--- app/models/pokemon/card.rb
+++ app/models/pokemon/card.rb
@@ -1,3 +1,3 @@
 class Pokemon::Card < ApplicationRecord
-  belongs_to :set
+  belongs_to :set, foreign_key: "pokemon_set_id", inverse_of: :cards
 end
--- app/models/pokemon/set.rb
+++ app/models/pokemon/set.rb
@@ -1,3 +1,3 @@
 class Pokemon::Set < ApplicationRecord
-  has_many :cards
+  has_many :cards, foreign_key: "pokemon_set_id", inverse_of: :set
 end
