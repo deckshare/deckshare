90f8b17e00c436536c783fd9eb4844268bd1a77a
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-02-23 18:32:04 -0800

    Pokemon::Card aggregation helpers

--- app/models/pokemon/card.rb
+++ app/models/pokemon/card.rb
@@ -1,3 +1,25 @@
 class Pokemon::Card < ApplicationRecord
   belongs_to :set, foreign_key: "pokemon_set_id", inverse_of: :cards
+
+  class << self
+    def regulation_marks
+      distinct.order(:regulation_mark).pluck(:regulation_mark)
+    end
+
+    def subtypes
+      joins('cross join unnest(subtypes) as subtype where subtype is not null')
+        .pluck(Arel.sql('array_agg(distinct subtype order by subtype asc)'))
+        .flatten
+    end
+
+    def supertypes
+      distinct.order(:supertype).pluck(:supertype)
+    end
+
+    def types
+      joins('cross join unnest(types) as ptype where ptype is not null')
+        .pluck(Arel.sql('array_agg(distinct ptype order by ptype asc)'))
+        .flatten
+    end
+  end
 end
