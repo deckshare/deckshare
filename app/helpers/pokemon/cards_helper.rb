ad7fe95bf754c1b3ed415dd4f144cce49ee6edf4
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-03-16 18:47:05 -0700

    Card image helpers

--- app/helpers/pokemon/cards_helper.rb
+++ app/helpers/pokemon/cards_helper.rb
@@ -2,5 +2,10 @@
 
 module Pokemon
   module CardsHelper
+    def card_image(card, size: :large)
+      raise ArgumentError, "#{size} must be :large or :small" unless %i[large small].include?(size)
+
+      image_tag(card.images[size], alt: card, title: card, class: 'img-fluid')
+    end
   end
 end
